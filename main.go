5ec24e51263e8185af34b6c3d01a718bd9f11b99
eminoz <eminoz@yahoo.com>
2023-04-04 00:37:53 +0300

    added v1

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/eminoz/logging/broker"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	f := fiber.New()
+	broker.Connect()
 	f.Get("/", func(c *fiber.Ctx) error { return c.JSON("hello") })
 
 	f.Listen(":3000")
